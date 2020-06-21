cbbb309d117cfadbc4e572e0a834bdaa2bf18282
Michael Arnold Owens <xmichael.owens@gmail.com>
2020-06-20 20:38:40 -0700

    use css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './styles/App.module.css';
 
 function App() {
+  console.log(styles)
+  const {App, AppLogo, AppHeader, AppLink} = styles;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={App}>
+      <header className={AppHeader}>
+        <img src={logo} className={AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
