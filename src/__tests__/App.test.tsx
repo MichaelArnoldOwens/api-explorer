dfcb180a3311fd58c38f3e36f11036a3d10351de
Michael Arnold Owens <xmichael.owens@gmail.com>
2020-06-20 23:05:48 -0700

    add enzyme for shallow rendering add snapshot test for App

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import App from "App";
+import React from "react";
+import { shallow } from "enzyme";
+
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
